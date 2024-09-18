83e1db62249bc16fa55c50d06d16da2347d671e5
Louis Herman <louisherman@live.be>
2024-09-18 02:14:36 +0200

    ADD(Character): GetPosition()

--- source/Character/Character.h
+++ source/Character/Character.h
@@ -9,6 +9,7 @@ public:
 	void Update();
 
 	const FpsCameraController& GetCameraController() const { return m_CameraController; }
+	const Float3& GetPosition() const { return m_CameraController.GetPosition(); }
 
 private:
 	FpsCameraController m_CameraController;
