65b87dc96f6dc811d48d2fd321d3441a2b9d5d1a
Louis Herman <louisherman@live.be>
2024-09-18 17:32:57 +0200

    ADD(ShadowRenderer): render to depthStencil

--- source/Renderer/ShadowRenderer.cpp
+++ source/Renderer/ShadowRenderer.cpp
@@ -1,7 +1,11 @@
 #include "pch.h"
 #include "ShadowRenderer.h"
 
+#include <Services\RenderSystems.h>
+#include <Rendering\State\Shader.h>
+
 using namespace TowerGame;
+using namespace Rendering;
 
 const Float3 ShadowRenderer::m_LightDir{ { Float3{-1,-1,1}.Normalized() } };
 
@@ -12,5 +16,13 @@ ShadowRenderer::ShadowRenderer()
 
 void ShadowRenderer::Render()
 {
+	m_DepthStencil.Clear();
+	ID3D11RenderTargetView* renderTargets[1]
+	{
+		nullptr
+	};
+	Globals::pGpu->GetContext().OMSetRenderTargets(1, renderTargets, m_DepthStencil.GetView());
+	RenderSystems::GetTextureRenderer().Render<Shader::Function::Vertex>();
+	RenderSystems::GetTerrainRenderer().Render<Shader::Function::Vertex>();
 }
 
